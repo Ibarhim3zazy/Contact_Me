ba0951706359e58c7c5f4851a22152ed02a35204
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2022-04-14 06:49:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector(".card-btn").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+function mail() {
+  window.open('mailto:Ibrahim3zazy@gmail.com?subject=تبرعات');
+}
+function messenger() {
+  window.open('http://m.me/.Ibrahim3zazy');
+}
+function whatsapp() {
+  window.open('https://wa.me/+2001145452440');
+}
+function telegram() {
+  window.open('https://t.me/Ibrahim3zazy');
+}
